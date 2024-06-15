036126d1ac5c28f2a5b2eabf27ce437335c7b058
LekhanLeafCraft <150507274+LekhanLeafCeaft@users.noreply.github.com>
2024-06-15 15:44:18 +0530

    Authentication added

---
+++ backend/api/auth.js
@@ -0,0 +1,61 @@
+const { Router } = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = Router();
+
+auth.use((req, res, next) => {
+    req.users = req.app.get('users');
+    next();
+});
+
+auth.post("/signup", async (req, res) => {
+    const { userType, userId, password } = req.body;
+    const users = req.users;
+
+    try {
+        const existingUser = await users.findOne({ userId: userId });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await users.insertOne({ userType, userId, password: hashedPassword });
+
+        res.status(201).json({
+            message: "User created successfully",
+            user: result.insertedId,
+        });
+    } catch (error) {
+        console.error("Signup error", error);
+        res.status(500).json({ message: "Failed to register user" });
+    }
+});
+
+auth.post("/login", async (req, res) => {
+    const { userType, userId, password } = req.body;
+    const users = req.users;
+    try {
+        const dbuser = await users.findOne({ userId: userId });
+        if (!dbuser) {
+            return res.status(400).json({ message: "Invalid username" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, dbuser.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+
+        const token = jwt.sign({ userId: dbuser.userId }, "secretkey", { expiresIn: "1d" });
+        res.json({
+            message: "Login successful",
+            token,
+            userType: dbuser.userType,
+            userId:dbuser.userId
+        });
+    } catch (error) {
+        console.error("Login error", error);
+        res.status(500).json({ message: "Failed to login" });
+    }
+});
+
+module.exports = auth;
---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs'); 
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const url = 'mongodb+srv://dhulikonaadmin:mmkpOkxtiISz8DBn@cluster0.12tcr9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbName = 'dhulikonaadmin';
+
+const client = new MongoClient(url);
+
+async function connect() {
+    try {
+        await client.connect();
+        console.log('Connected successfully to MongoDB');
+
+        const db = client.db(dbName);
+        // // // Perform actions on the collection object
+        const users = db.collection('users')
+        const pumpoperater = db.collection('pumpoperater')
+
+
+        app.set('users', users)
+        app.set('pumpoperater', pumpoperater)
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+connect();
+
+const userApp = require('./api/userroute')
+const auth = require('./api/auth')
+app.use('/users', userApp)
+app.use('/', auth)
+
+app.listen(5000, ()=> console.log("server running on port 5000"));
