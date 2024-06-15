de7521a5ac6687d502bae453d2e6f64ba3226fd6
koushik <koushik00500@gmail.com>
2024-06-15 14:48:07 +0530

    Auth Layout added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import PumpOperater from './pages/PumpOperater';
+import Layout from './components/Layout';
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Layout/>
+    },
+    {
+      path:'login',
+      element:<Login/>
+    },
+    {
+      path:'register',
+      element:<Register/>
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <RouterProvider router={router}/>
+    </section>
   );
 }
 
---
+++ src/pages/PumpOperater.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PumpOperater() {
+  return (
+    <div>
+      <h1>Pump Operater</h1>
+    </div>
+  )
+}
+
+export default PumpOperater
